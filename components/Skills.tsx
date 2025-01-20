a103ccefa48cc30476069bf622eb15830edcc3fe
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-01-20 14:55:38 +0100

    Refactor: Adjust skill categories and icons for improved layout and consistency with global styles.

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,170 +1,105 @@
 "use client";
 import React from "react";
-import { HoverEffect } from "./ui/CardHoverEffect";
 import {
-  FaDatabase,
   FaReact,
   FaNodeJs,
   FaHtml5,
   FaCss3,
   FaJs,
   FaGit,
-  FaPython,
+  FaJava,
 } from "react-icons/fa";
 import {
+  SiSpring,
+  SiSpringboot,
+  SiApachemaven,
   SiDotnet,
-  SiTailwindcss,
-  SiExpress,
+  SiCsharp,
   SiTypescript,
   SiNextdotjs,
-  SiMui,
-  SiCsharp,
+  SiTailwindcss,
+  SiPostgresql,
+  SiMicrosoftsqlserver,
+  SiExpress,
   SiFramer,
   SiShadcnui,
-  SiMicrosoftsqlserver,
-  SiPostgresql,
+  SiMui,
+  SiMysql,
 } from "react-icons/si";
 
-const skills: {
-  title: string;
-  description: string;
-  link: string;
-  icon: JSX.Element;
-}[] = [
+const skills = [
   {
-    title: "JavaScript, HTML, CSS",
-    description: "The holy trinity of web dev",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Guide",
-    icon: (
-      <>
-        <FaJs size={25} />
-        <FaHtml5 size={25} />
-        <FaCss3 size={25} />
-      </>
-    ),
+    category: "Programming Languages",
+    items: [
+      { name: "JavaScript", icon: <FaJs size={30} /> },
+      { name: "TypeScript", icon: <SiTypescript size={30} /> },
+      { name: "Java", icon: <FaJava size={30} /> },
+      { name: "C#", icon: <SiCsharp size={30} /> },
+    ],
   },
   {
-    title: "React.js",
-    description: "Because modern problems need modern solutions",
-    link: "https://reactjs.org/docs/getting-started.html",
-    icon: <FaReact size={40} />,
+    category: "Frontend Development",
+    items: [
+      { name: "React.js", icon: <FaReact size={30} /> },
+      { name: "Next.js", icon: <SiNextdotjs size={30} /> },
+      { name: "HTML", icon: <FaHtml5 size={30} /> },
+      { name: "CSS", icon: <FaCss3 size={30} /> },
+      { name: "Tailwind CSS", icon: <SiTailwindcss size={30} /> },
+      { name: "Material-UI", icon: <SiMui size={30} /> },
+      { name: "ShadCN UI", icon: <SiShadcnui size={30} /> },
+    ],
   },
   {
-    title: "Next.js",
-    description: "The React Framework for Production",
-    link: "https://nextjs.org/docs",
-    icon: <SiNextdotjs size={40} />,
+    category: "Backend Development",
+    items: [
+      { name: "Spring Framework", icon: <SiSpring size={30} /> },
+      { name: "Spring Boot", icon: <SiSpringboot size={30} /> },
+      { name: "ASP.NET Core", icon: <SiDotnet size={30} /> },
+    ],
   },
   {
-    title: "TypeScript",
-    description: "Typed JavaScript at Any Scale",
-    link: "https://www.typescriptlang.org/docs/",
-    icon: <SiTypescript size={40} />,
+    category: "Database & Tools",
+    items: [
+      { name: "MS SQL Server", icon: <SiMicrosoftsqlserver size={30} /> },
+      { name: "MySQL", icon: <SiMysql size={30} /> },
+      { name: "PostgreSQL", icon: <SiPostgresql size={30} /> },
+      { name: "Maven", icon: <SiApachemaven size={30} /> },
+      { name: "Git", icon: <FaGit size={30} /> },
+    ],
   },
   {
-    title: "C#",
-    description: "My superpower",
-    link: "https://docs.microsoft.com/en-us/dotnet/csharp/",
-    icon: <SiCsharp size={40} />,
-  },
-  {
-    title: "ASP.NET Core MVC",
-    description: "Building robust web apps with style",
-    link: "https://docs.microsoft.com/en-us/aspnet/core/mvc/overview?view=aspnetcore-5.0",
-    icon: <SiDotnet size={40} />,
-  },
-  {
-    title: "Entity Framework",
-    description: "Data access made easy",
-    link: "https://docs.microsoft.com/en-us/ef/",
-    icon: <FaDatabase size={40} />,
-  },
-
-  {
-    title: "MS SQL Server, Postgres",
-    description: "Where all the data magic happens",
-    link: "https://docs.microsoft.com/en-us/sql/sql-server/?view=sql-server-ver15",
-    icon: (
-      <>
-        <SiMicrosoftsqlserver size={40} />
-        <SiPostgresql size={40} />
-      </>
-    ),
-  },
-  {
-    title: "Git",
-    description: "Version control ninja",
-    link: "https://git-scm.com/doc",
-    icon: <FaGit size={40} />,
-  },
-
-  {
-    title: "Node.js",
-    description: "JavaScript runtime built on Chrome's V8 engine",
-    link: "https://nodejs.org/en/docs/",
-    icon: <FaNodeJs size={40} />,
-  },
-  {
-    title: "Express.js",
-    description: "Fast, unopinionated, minimalist web framework for Node.js",
-    link: "https://expressjs.com/en/starter/installing.html",
-    icon: <SiExpress size={40} />,
-  },
-  {
-    title: "Tailwind CSS",
-    description: "Utility-first CSS framework for rapid UI development",
-    link: "https://tailwindcss.com/docs",
-    icon: <SiTailwindcss size={40} />,
-  },
-  {
-    title: "ShadCN UI",
-    description:
-      "A collection of reusable UI components built with Radix UI and Tailwind CSS",
-    link: "https://shadcn.dev/",
-    icon: <SiShadcnui size={40} />,
-  },
-  {
-    title: "Material-UI",
-    description: "React components for faster and easier web development",
-    link: "https://mui.com/getting-started/usage/",
-    icon: <SiMui size={40} />,
-  },
-  {
-    title: "Framer Motion",
-    description: "A production-ready motion library for React",
-    link: "https://www.framer.com/motion/",
-    icon: <SiFramer size={40} />,
-  },
-
-  {
-    title: "Python",
-    description: "The versatile powerhouse",
-    link: "https://docs.python.org/3/",
-    icon: <FaPython size={40} />,
-  },
-  {
-    title: "Data Structures and Algorithms",
-    description: "Brain food",
-    link: "https://en.wikipedia.org/wiki/Data_structure",
-    icon: <FaDatabase size={40} />,
-  },
-  {
-    title: "Object-Oriented Programming",
-    description: "Thinking in objects is my thing",
-    link: "https://en.wikipedia.org/wiki/Object-oriented_programming",
-    icon: <FaDatabase size={40} />,
-  },
-  {
-    title: "Adaptability & Problem-solving",
-    description: "My middle names",
-    link: "https://en.wikipedia.org/wiki/Problem_solving",
-    icon: <FaDatabase size={40} />,
+    category: "Other Skills",
+    items: [
+      { name: "Data Structures & Algorithms", icon: <FaJs size={30} /> },
+      { name: "Framer Motion", icon: <SiFramer size={30} /> },
+      { name: "Object-Oriented Programming", icon: <FaJs size={30} /> },
+    ],
   },
 ];
+
 export default function Skills() {
   return (
     <>
-      <HoverEffect items={skills} />
+      <div className="flex justify-center items-center">
+        <h2 className="text-3xl lg:text-5xl font-bold mb-4 text-white">
+          My <span className="text-blue-600"> toolbox</span>
+        </h2>
+      </div>
+      <div className="skills-section">
+        {skills.map((skillGroup, index) => (
+          <div key={index} className="skill-group">
+            <h3 className="skill-category">{skillGroup.category}</h3>
+            <div className="skill-items">
+              {skillGroup.items.map((skill, idx) => (
+                <div key={idx} className="skill-item">
+                  {skill.icon}
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
